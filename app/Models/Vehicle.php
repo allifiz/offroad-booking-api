a95097624033b6f6120dea600f78fd0b1861f9ac
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-07-17 21:43:41 +0700

    refactor: enable vehicle factory

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\Enums\VehicleStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'plate_number',
