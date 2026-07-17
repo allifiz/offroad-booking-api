dd52a0dd947f3cd8f8556968f439f7a5b516e1a5
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-07-17 21:43:20 +0700

    refactor: enable driver profile factory

--- app/Models/DriverProfile.php
+++ app/Models/DriverProfile.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Enums\DriverStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DriverProfile extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'status',
