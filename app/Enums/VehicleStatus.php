3ea8c1a64173adddc3a454eabf4a0511123fc505
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-07-17 22:19:40 +0700

    feat: add unavailable vehicle status

--- app/Enums/VehicleStatus.php
+++ app/Enums/VehicleStatus.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum VehicleStatus: string
 {
     case AVAILABLE = 'available';
+    case UNAVAILABLE = 'unavailable';
     case IN_USE = 'in_use';
     case MAINTENANCE = 'maintenance';
     case INACTIVE = 'inactive';
